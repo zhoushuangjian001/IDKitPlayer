45d867763eaf11037bd5fc30243c07ec2b9141e5
Network小贱 <zhoushuangjian511@163.com>
2019-02-28 16:49:01 +0800

    Initial Commit

---
+++ IDKitPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IDKitPlayer
+//
+//  Created by Mac on 2019/2/28.
+//  Copyright © 2019 Network小贱. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
